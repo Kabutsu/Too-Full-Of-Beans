56b1e8d29eb8d833dc8a127300fe8e483c7e64a3
Sam Law <samuel.law@system1group.com>
2024-01-28 11:24:30 +0000

    Record score in UI

--- Assets/Scripts/Controllers/TrumpetController.cs
+++ Assets/Scripts/Controllers/TrumpetController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,6 +7,9 @@ namespace Assets.Scripts.Controllers
 {
     public class TrumpetController : MonoBehaviour
     {
+        public GameObject TextObject;
+        private TextMeshProUGUI Text;
+
         public Vector2 FireRumble = new(0.5f, 3f);
 
         [SerializeField]
@@ -14,12 +18,15 @@ namespace Assets.Scripts.Controllers
         [SerializeField]
         private float TimeBetweenSpawns = 0.125f;
         private int BeansToSpawn = 0;
+        private int BeansSpawned = 0;
         private bool IsSpawning = false;
 
         private Vector3 BeanSpawnCoordinates = new(8.35f, 1.5f, -3f);
 
         private void Start()
         {
+            Text = TextObject.GetComponent<TextMeshProUGUI>();
+
             if (transform.position.x < 0)
                 BeanSpawnCoordinates.x = -BeanSpawnCoordinates.x;
         }
@@ -41,7 +48,9 @@ namespace Assets.Scripts.Controllers
             while (BeansToSpawn > 0)
             {
                 Instantiate(Bean, BeanSpawnCoordinates, transform.rotation);
+                BeansSpawned++;
                 BeansToSpawn--;
+                Text.text = BeansSpawned.ToString();
 
                 Gamepad.current.SetMotorSpeeds(FireRumble.x, FireRumble.y);
                 yield return new WaitForSeconds(TimeBetweenSpawns / 1.5f);
